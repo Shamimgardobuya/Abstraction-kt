8894995b094545766c417242eed6524292ed6e30
Shamim Gard Obuya <99389344+Shamimgardobuya@users.noreply.github.com>
2022-08-13 10:07:14 +0300

    Update main.kt

--- src/main.kt
+++ src/main.kt
@@ -21,3 +21,23 @@ class Person(name: String):Student(){
     }
 }
 
+fun planets(name:String){
+    println("Enter the name of the heavenly body")
+//     var name=readline()!!.toString()
+    when(name) {
+        "Sun" -> println("Is a star")
+        "Moon" -> println("Moon comes out at night")
+        "Earth" -> println("Is the third planet")
+        else -> println("This planet does not exists")
+    }
+
+}
+//create a function for using when as an expression
+fun calculate(number:Int){
+    var number2=when(number){
+        1->println("This is number one")
+        2 ->println("This is number two")
+        4 ->println("This is number four")
+        else ->println("This number is greater than 3 ")
+    }
+}
